92faffe51669822684674057bf84e9c096e33187
Mathias231 <mathias.krogh@outlook.com>
2023-05-22 17:04:08 +0200

    init

---
+++ components/navbar.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import React from 'react';
+
+function Navbar() {
+  return (
+    <>
+      <header className="bg-gray-800 py-4">
+        <div className=" flex items-center w-full justify-between">
+          <div className="flex items-center ml-4">
+            <Link
+              href="/"
+              className="bg-gray-700  hover:bg-gray-600 text-gray-300 hover:text-white text-xl font-medium py-2 px-4 rounded"
+              title="Hjemmeside"
+            >
+              Hjem
+            </Link>
+          </div>
+          <div className="flex items-center mr-4">
+            <div className="mr-4">
+              {/* {session && (
+                <Image
+                  className="h-10 w-10 rounded-full"
+                  src={session.user.image || ''}
+                  alt="Profile Image"
+                  width={10}
+                  height={10}
+                />
+              )} */}
+            </div>
+            <div className="text-gray-300 mr-4">
+              {/* {session && (
+                <p className="text-sm font-medium">{session.user.name}</p>
+              )} */}
+            </div>
+            {/* {session ? <SignOutButton /> : <SignInButton />} */}
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
