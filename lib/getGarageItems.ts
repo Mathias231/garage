ae2bc80e2f7f2fa81a2a46272986469921252e64
Mathias231 <mathias.krogh@outlook.com>
2023-05-23 18:01:00 +0200

    Fetching GarageItems

--- lib/getGarageItems.ts
+++ lib/getGarageItems.ts
@@ -1,12 +1,13 @@
 import useSWR from 'swr';
 import { fetcher } from './fetcher';
+import { IGarage } from '@/types/garage.types';
 
-export default function GetGarageItems() {
-  const { data, error, mutate } = useSWR('/api/garage', () => {
-    fetcher('/api/garage');
-  });
+export default function GetGarageItems(garageId: string) {
+  const { data, error, mutate } = useSWR(`/api/garage/${garageId}`, () =>
+    fetcher<IGarage>(`/api/garage/${garageId}`),
+  );
 
   let isLoading = !data && !error;
 
-  return { garage: data };
+  return { garage: data, error, isLoading, mutate };
 }
