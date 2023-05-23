6eeb404dbd977f139e104a55bead776ce2a100cd
Mathias231 <mathias.krogh@outlook.com>
2023-05-23 17:59:48 +0200

    Added Icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import SignOutButton from './session/signOut-button';
 import SignInButton from './session/signIn-button';
+import { FaPlus } from 'react-icons/fa';
 
 function Navbar() {
   const { data: session } = useSession();
@@ -22,7 +23,7 @@ function Navbar() {
             </Link>
             {session?.user.whiteListed === true ? (
               <Link
-                href="/garage"
+                href="/garage/"
                 className="bg-gray-700  hover:bg-gray-600 text-gray-300 hover:text-white text-xl font-medium py-2 px-4 rounded"
                 title="Garasje"
               >
@@ -34,6 +35,15 @@ function Navbar() {
           </div>
           <div className="flex items-center mr-4">
             <div className="mr-4">
+              <Link href="/garage/items/create" title="Legg til verktøy">
+                <FaPlus
+                  style={{ color: '#ffffff' }}
+                  className="translate-x-2"
+                  size={40}
+                />
+              </Link>
+            </div>
+            <div className="mr-4">
               {session && (
                 <Image
                   className="h-10 w-10 rounded-full"
@@ -44,6 +54,7 @@ function Navbar() {
                 />
               )}
             </div>
+
             <div className="text-gray-300 mr-4">
               {session && (
                 <p className="text-sm font-medium">{session.user.name}</p>
