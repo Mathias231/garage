2a0672f1559b98401ff4eaa20d384e6ae0621ecf
Mathias231 <mathias.krogh@outlook.com>
2023-05-24 20:02:15 +0200

    Added DeleteButton & Refresh Item List

--- pages/garage/index.tsx
+++ pages/garage/index.tsx
@@ -1,10 +1,12 @@
-import GetGarageItems from '@/lib/getGarageItems';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaTrash } from 'react-icons/fa';
+import GetGarageWithItems from '@/lib/getGarageWithItems';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export default function Garage() {
   const { data: session } = useSession();
@@ -15,21 +17,32 @@ export default function Garage() {
     if (!session?.user.whiteListed) router.push('/');
   });
 
-  // Mathias's garage ID
-  const DB_ID = '646c92b21f1d71f34789ba38';
-
-  // Fetching items in garage
-  const { garage, isLoading, mutate } = GetGarageItems(
-    typeof DB_ID === 'string' ? DB_ID : '1',
-  );
+  const { garage, isLoading, mutate } = GetGarageWithItems();
 
   if (!garage) return <div>No items found</div>;
+
+  const handleClick = async (itemId: string) => {
+    await axios
+      .delete(`/api/item/${itemId}`)
+      .then((msg) => {
+        mutate();
+        toast.success('Gjenstand Slettet!', {
+          position: 'top-center',
+        });
+      })
+      .catch((err) => {
+        toast.warn(err, {
+          position: 'top-center',
+        });
+      });
+  };
 
   return (
     <main>
       <div className="bg-teal-300 flex justify-center items-center">
         <div className="p-4">
           <h1 className="text-xl font-bold">{garage.name}</h1>
+          {}
         </div>
       </div>
       <div className="flex justify-center mt-2">
@@ -41,9 +54,9 @@ export default function Garage() {
         <div className="bg-white">
           <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {garage.items.map((item, i) => {
+              {garage.items.map((item) => {
                 return (
-                  <a key={i} className="border">
+                  <a key={item.id} className="border rounded-md p-1 shadow-lg">
                     <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                       <Image
                         src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
@@ -55,14 +68,24 @@ export default function Garage() {
                       />
                     </div>
                     <h3 className="mt-1 text-lg font-medium text-gray-900 ">
-                      Earthen Bottle
+                      {item.name}
                     </h3>
                     <p className="mt-4 text-sm text-gray-700">
                       Weight: {item.weight}kg
                     </p>
+                    {item.id}
                     <p className="mt-4 text-sm text-gray-700">
                       Durability: {item.durability}%
                     </p>
+                    {session?.user.userId === item.userId ? (
+                      <div>
+                        <button onClick={() => handleClick(item.id)}>
+                          <FaTrash />
+                        </button>
+                      </div>
+                    ) : (
+                      ''
+                    )}
                   </a>
                 );
               })}
