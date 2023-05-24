b815a2675c055bccbed29894ab04d8724650c9de
Mathias231 <mathias.krogh@outlook.com>
2023-05-24 20:01:15 +0200

    Including more items

--- pages/api/garage/index.ts
+++ pages/api/garage/index.ts
@@ -5,14 +5,15 @@ import { createRouter } from 'next-connect';
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
 router.post(async (req, res) => {
-  const { userId, garageName, color } = req.body;
+  const { userId, garageName } = req.body;
 
   const createGarage = await prisma.garage
     .create({
       data: {
         userId: userId,
         name: garageName,
-        garageColor: color,
+        dateCreated: new Date(),
+        dateUpdated: new Date(),
       },
     })
     .catch((err) => {
@@ -20,6 +21,32 @@ router.post(async (req, res) => {
     });
 
   res.status(200).send('Garage Created!');
+});
+
+router.get(async (req, res) => {
+  const getGarage = await prisma.garage
+    .findFirst({
+      include: {
+        items: {
+          select: {
+            id: true,
+            userId: true,
+            garageId: true,
+            name: true,
+            category: true,
+            durability: true,
+            weight: true,
+            dateCreated: true,
+            dateUpdated: true,
+          },
+        },
+      },
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+
+  res.status(200).send(getGarage);
 });
 
 export default router.handler({
