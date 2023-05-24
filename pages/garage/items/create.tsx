c634ee65fcc75435e861b52e916b139faface5ac
Mathias231 <mathias.krogh@outlook.com>
2023-05-24 19:59:58 +0200

    Updating

--- pages/garage/items/create.tsx
+++ pages/garage/items/create.tsx
@@ -1,9 +1,26 @@
 import Tools from '@/components/item/tools';
 import Vehicle from '@/components/item/vehicle';
-import React, { useState } from 'react';
+import GetGarageWithItems from '@/lib/getGarageWithItems';
+import GarageId from '@/pages/api/garage/[garageId]';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react';
+
 import { FaPhotoVideo } from 'react-icons/fa';
 function Create() {
+  const { data: session } = useSession();
+  const { garage, isLoading, mutate } = GetGarageWithItems();
   const [toggle, setToggle] = useState<boolean>(true);
+
+  const router = useRouter();
+
+  // If user is not whiteListed, send back to startpage
+  useEffect(() => {
+    if (!session) router.push('/');
+  });
+
+  if (!session?.user.userId) return <div>Loading Session...</div>;
+  if (!garage) return <div>No items found</div>;
 
   return (
     <div>
@@ -23,7 +40,13 @@ function Create() {
           Kjøretøy
         </button>
       </div>
-      <div>{toggle ? <Tools /> : <Vehicle />}</div>
+      <div>
+        {toggle ? (
+          <Tools garageId={garage.id} userId={session?.user.userId} />
+        ) : (
+          <Vehicle />
+        )}
+      </div>
     </div>
   );
 }
