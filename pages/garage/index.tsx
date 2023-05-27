51c60fe0ecb4dfaf06007109e49b9846b39ec5cf
Mathias <mathias.krogh@outlook.com>
2023-05-27 18:19:22 +0200

    Moving code and importing chilld component

--- pages/garage/index.tsx
+++ pages/garage/index.tsx
@@ -1,12 +1,11 @@
 import { useSession } from 'next-auth/react';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { FaPlus, FaTrash } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
 import GetGarageWithItems from '@/lib/getGarageWithItems';
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import Item from '@/components/insideGarage/item';
+import VehicleList from '@/components/insideGarage/vehicle';
 
 export default function Garage() {
   const { data: session } = useSession();
@@ -21,22 +20,6 @@ export default function Garage() {
   if (isLoading) return <div>Loading...</div>;
   if (!garage) return <div>No items found</div>;
 
-  const handleClick = async (itemId: string) => {
-    await axios
-      .delete(`/api/item/${itemId}`)
-      .then((msg) => {
-        mutate();
-        toast.success('Gjenstand Slettet!', {
-          position: 'top-center',
-        });
-      })
-      .catch((err) => {
-        toast.warn(err, {
-          position: 'top-center',
-        });
-      });
-  };
-
   return (
     <main>
       <div className="bg-teal-300 flex justify-center items-center">
@@ -45,48 +28,49 @@ export default function Garage() {
         </div>
       </div>
       <div className="flex justify-center mt-2">
-        <Link href="/garage/items/create" title="Legg til verktøy">
+        <Link href="/garage/create/" title="Legg til verktøy">
           <FaPlus size={40} className="hover:translate-y-1" />
         </Link>
       </div>
       <div>
         <div className="bg-white">
-          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {garage.items.map((item) => {
-                return (
-                  <a key={item.id} className="border rounded-md p-1 shadow-lg">
-                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                      <Image
-                        src={`/api/image/${item.image.internalName}`}
-                        alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                        className="h-full w-full object-cover object-center group-hover:opacity-75"
-                        width={1000}
-                        height={1000}
-                        priority
-                      />
-                    </div>
-                    <h3 className="mt-1 text-lg font-medium text-gray-900 ">
-                      {item.name}
-                    </h3>
-                    <p className="mt-4 text-sm text-gray-700">
-                      Vekt: {item.weight}kg
-                    </p>
-                    <p className="mt-4 text-sm text-gray-700">
-                      Varighet: {item.durability}%
-                    </p>
-                    {session?.user.userId === item.userId ? (
-                      <div>
-                        <button onClick={() => handleClick(item.id)}>
-                          <FaTrash />
-                        </button>
-                      </div>
-                    ) : (
-                      ''
-                    )}
-                  </a>
-                );
-              })}
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl space-y-20 lg:px-8">
+            <div>
+              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                {garage.items.map((item) => {
+                  return (
+                    <Item
+                      key={item.id}
+                      id={item.id}
+                      userId={item.userId}
+                      garageId={item.garageId}
+                      name={item.name}
+                      category={item.category}
+                      durability={item.durability}
+                      image={item.image}
+                      weight={item.weight}
+                    />
+                  );
+                })}
+              </div>
+              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                {garage.vehicle?.map((vehicle) => {
+                  return (
+                    <VehicleList
+                      key={vehicle.id}
+                      id={vehicle.id}
+                      userId={vehicle.userId}
+                      garageId={vehicle.garageId}
+                      name={vehicle.name}
+                      category={vehicle.category}
+                      model={vehicle.model}
+                      image={vehicle.image}
+                      color={vehicle.color}
+                      distanceDriven={vehicle.distanceDriven}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
