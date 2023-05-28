c573c1817aae5557819afe35fe9b76a5e35f7979
Mathias <mathias.krogh@outlook.com>
2023-05-28 18:40:22 +0200

    Changing Language

--- components/loading/loadingScreen.tsx
+++ components/loading/loadingScreen.tsx
@@ -4,7 +4,7 @@ const LoadingScreen = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-5 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold">Loading...</h1>
+        <h1 className="text-2xl font-bold">Laster inn...</h1>
         <div className="mt-4">
           <div className="animate-pulse h-4 bg-gray-300 rounded"></div>
           <div className="animate-pulse mt-2 h-4 bg-gray-300 rounded"></div>
