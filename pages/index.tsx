c380c5f14faa233e1e2ec9bc5725bdd720dcc9d9
Mathias <mathias.krogh@outlook.com>
2023-05-29 16:36:50 +0200

    Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-import { useSession } from 'next-auth/react';
+import GetGarageWithItems from '@/lib/getGarageWithItems';
 
 export default function Home() {
-  return <main></main>;
+  const { garage, isLoading, mutate } = GetGarageWithItems();
+  if (!garage) return <div>Garasjen er tom...</div>;
+
+  const totalLength = garage?.items.length + garage?.vehicle.length;
+
+  return (
+    <main>
+      <div className="flex flex-col items-center justify-center min-h-screen h-64 bg-gray-100">
+        <h1 className="text-3xl font-bold mb-4">Welcome to My Garage!</h1>
+        <div className="bg-white rounded-lg p-4 shadow-lg">
+          <p className="text-lg mb-2 font-semibold">
+            Total Items in Garage: {totalLength}
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
